ea9aeb1761adac8c8dfead547843b9c4e9c64a67
Daisy-GENG <janeandrebecca@outlook.com>
2022-11-13 11:39:38 +0000

    backbone selector

--- models/backbones/backbone_selector.py
+++ models/backbones/backbone_selector.py
@@ -12,3 +12,25 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
+from models.backbones.resnet.resnet_backbone import ResNetBackbone
+
+from utils.tools.logger import Logger as Log
+
+
+class BackboneSelector(object):
+
+    def __init__(self, configer):
+        backbone = self.configer.get('network', 'backbone')
+
+    def get_backbone(self, **params):
+        backbone = self.configer.get('network', 'backbone')
+
+        model = None
+        if ('resnet' in backbone or 'resnext' in backbone or 'resnest' in backbone) and 'senet' not in backbone:
+            model = ResNetBackbone(self.configer)(**params)
+        else:
+            Log.error('Backbone: {} is invalid.'.format(backbone))
+            exit(1)
+
+        return model
