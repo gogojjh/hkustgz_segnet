79131e9a3e02c0a85beb9cb4dafed4fbf3bc37f4
Daisy-GENG <janeandrebecca@outlook.com>
2022-11-13 09:36:12 +0000

    network from eth and trainer and main

---
+++ models/backbones/backbone_selector.py
@@ -0,0 +1,14 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Created by: Donny You, RainbowSecret
+# Microsoft Research
+# yuyua@microsoft.com
+# Copyright (c) 2019
+##
+# This source code is licensed under the MIT-style license found in the
+# LICENSE file in the root directory of this source tree
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
