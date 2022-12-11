959c43e035a974d2d8d9051bd771f66f786bc4a1
Daisy-GENG <janeandrebecca@outlook.com>
2022-12-11 03:57:10 +0000

    sinkhorn gaussian from github

--- lib/models/modules/sinkhorn.py
+++ lib/models/modules/sinkhorn.py
@@ -10,11 +10,11 @@ def distributed_sinkhorn(out, sinkhorn_iterations=3, epsilon=0.05):
 
     # make the matrix sums to 1
     sum_L = torch.sum(L)
-    L /= sum_L
+    L /= sum_L  # mu_s, mu_t constraint
 
     for _ in range(sinkhorn_iterations):
         L /= torch.sum(L, dim=1, keepdim=True)
-        L /= K
+        L /= K  # ! equipartition constraint + uniform distribution
 
         L /= torch.sum(L, dim=0, keepdim=True)
         L /= B
@@ -80,10 +80,45 @@ def distributed_greenkhorn(out, sinkhorn_iterations=100, epsilon=0.05):
 
     L = L.t()
 
-    indexs = torch.argmax(L, dim=1) #! which protoype in this class to select
+    indexs = torch.argmax(L, dim=1)  # ! which protoype in this class to select
     G = F.gumbel_softmax(L, tau=0.5, hard=True)
 
     return L, indexs
+
+# todo
+
+
+class SinkhornOT(torch.autograd.Function):
+    """ 
+    https://github.com/t-vi/pytorch-tvmisc/blob/master/wasserstein-distance/Pytorch_Wasserstein.ipynb
+    """
+    @staticmethod
+    def forward(ctx, mu, nu, dist, lam=1e-3, N=100):
+        assert mu.dim() == 2 and nu.dim() == 2 and dist.dim() == 2
+        bs = mu.size(0)
+        d1, d2 = dist.size()
+        assert nu.size(0) == bs and mu.size(1) == d1 and nu.size(1) == d2
+        log_mu = mu.log()
+        log_nu = nu.log()
+        log_u = torch.full_like(mu, -math.log(d1))
+        log_v = torch.full_like(nu, -math.log(d2))
+        for i in range(N):
+            log_v = sinkstep(dist, log_nu, log_u, lam)
+            log_u = sinkstep(dist.t(), log_mu, log_v, lam)
+
+        # this is slight abuse of the function. it computes (diag(exp(log_u))*Mt*exp(-Mt/lam)*diag(exp(log_v))).sum()
+        # in an efficient (i.e. no bxnxm tensors) way in log space
+        distances = (-sinkstep(-dist.log()+dist/lam, -
+                     log_v, log_u, 1.0)).logsumexp(1).exp()
+        ctx.log_v = log_v
+        ctx.log_u = log_u
+        ctx.dist = dist
+        ctx.lam = lam
+        return distances
+
+    @staticmethod
+    def backward(ctx, grad_out):
+        return grad_out[:, None] * ctx.log_u * ctx.lam, grad_out[:, None] * ctx.log_v * ctx.lam, None, None, None
 
 
 if __name__ == "__main__":
